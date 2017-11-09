b21640be5ba30b9a23c61d51a86ad2570c1cab0f
menase12 <menase.yonatan@gmail.com>
2017-11-09 16:54:03 -0500

    Initial commit

---
+++ src/main/java/com/example/adressbook/AddressBook.java
@@ -0,0 +1,62 @@
+package com.example.adressbook;
+
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class AddressBook {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    @Size (min=2)
+    private String name;
+    @NotNull
+    @Email
+    private String email;
+    @NotEmpty
+    @Size(min=10)
+    private String phoneNumber;
+    
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name=name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email=email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber=phoneNumber;
+    }
+}
---
+++ src/main/java/com/example/adressbook/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.example.adressbook;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<AddressBook,Long> {
+
+}
